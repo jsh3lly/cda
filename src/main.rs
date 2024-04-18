67031d685f7de4a7a4096283cdd6298523b03da5
Jyotirmay Shelly <jyotirma@ualberta.ca>
2024-04-18 19:34:41 -0400

    Refactored code a bit and added a tests.rs file. Will write tests now.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::{env::{self, current_dir}, path::Path};
-use clap::{arg, command, value_parser, ArgAction, Command};
+mod tests;
+
+use std::env::current_dir;
+use clap::{arg, command, value_parser};
 use std::process::ExitCode;
 
+#[derive(PartialEq)]
 enum DirectionToSearch {
     RightToLeft,
     LeftToRight
+}
+
+struct Args {
+    ancestor_to_cd: String,
+    occurence: u8,
+    direction_to_search: DirectionToSearch
 }
 
 fn main() -> ExitCode {
@@ -15,48 +24,67 @@ fn main() -> ExitCode {
         .arg(arg!(-r --reverse "Reverse order of search (starting from farthest to closest)"))
         .get_matches();
 
-    let ancestor_to_cd = matches.get_one::<String>("ANCESTOR").unwrap();
+    let ancestor_to_cd = matches.get_one::<String>("ANCESTOR").unwrap().clone();
 
-    let mut occurence_number : u8;
-    match matches.get_one::<u8>("OCCURENCE") {
-        Some(a) => {
-            occurence_number = *a;
-        }
-        None => {
-            occurence_number = 1;
-        }
-    }
+    let occurence_number = match matches.get_one::<u8>("OCCURENCE") {
+        Some(a) => *a,
+        None => 1
+    };
 
     let direction_to_search : DirectionToSearch = match matches.get_one::<bool>("reverse").unwrap() {
         true => DirectionToSearch::LeftToRight,
         false => DirectionToSearch::RightToLeft
     };
 
+    let args = Args {ancestor_to_cd, occurence: occurence_number, direction_to_search};
 
     let cwd = current_dir().expect("Either Current Working Directory doesn't exist or you don't have sufficient permissions to view it.");
-    let mut ancestors : Vec<&str> = cwd.to_str().unwrap().split("/").filter(|x| !x.is_empty()).collect();
+    match cda_core(cwd.to_str().unwrap(), args) {
+        Ok(path) => {
+            println!("{}", path);
+            ExitCode::from(0)
+        }
+        Err(()) => {
+            println!("[ERROR] Ancestor was not found!");
+            ExitCode::from(1)
+        }
+    }
+}
+
+fn cda_core(cwd: &str, args : Args) -> Result<String, ()> {
+    let ancestors : Vec<&str> = cwd.split("/").filter(|x| !x.is_empty()).collect();
     let ancestors_len = ancestors.len();
-    let mut idx = 0;
     let mut is_ancestor_found = false;
 
-    let ancestors_iterator : Vec<&str> = match direction_to_search {
+    let ancestors_iterator : Vec<&str> = match args.direction_to_search {
         DirectionToSearch::RightToLeft => ancestors.clone().into_iter().rev().collect(),
         DirectionToSearch::LeftToRight => ancestors.clone().into_iter().collect(),
     };
+
+    let mut idx = 0;
+    let mut curr_occurence = args.occurence;
     for possible_ancestor in ancestors_iterator {
-        if possible_ancestor.contains(ancestor_to_cd) {
-            if occurence_number == 1 {
+        // // special case when there is an ancestor called `node` BUT current dir is ALSO called
+        // // `node`. In such case, go to the ancestor instead of just being where you are.
+        // if direction_to_search == DirectionToSearch::RightToLeft
+        //     && idx == 0 {
+        //         idx += 1;
+        //         continue;
+        //     }
+        //
+        if possible_ancestor.contains(&args.ancestor_to_cd) {
+            if curr_occurence == 1 {
                 is_ancestor_found = true;
                 break;
             }
             else {
-                occurence_number -= 1;
+                curr_occurence -= 1;
             }
         }
         idx += 1;
     }
 
-    let path : String = match direction_to_search {
+    let path : String = match args.direction_to_search {
         DirectionToSearch::RightToLeft => {
             "/".to_string() + &ancestors[0..ancestors_len-idx].join("/")
         }
@@ -65,11 +93,8 @@ fn main() -> ExitCode {
         }
     };
 
-    if is_ancestor_found == false {
-        println!("Ancestor was not found!");
-        return ExitCode::from(1)
+    match is_ancestor_found {
+        true => Ok(path),
+        false => Err(())
     }
-
-    println!("{}", path);
-    return ExitCode::from(0);
 }
